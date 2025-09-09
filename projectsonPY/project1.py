461cec4aba5aed4aa91fc2342526ee5236c645f7
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-09-09 20:27:41 +0530

    Changes been made

--- projectsonPY/project1.py
+++ projectsonPY/project1.py
@@ -1,15 +1,33 @@
-
-input = (input("Enter a number: "))
-computer= -1
-dictonary={"snake":0,"water":1,"gun":2}
+import random
+input = (input("Enter snake, water or gun: "))
+computer= random.choice([0,1,-1])
+dictonary={"snake":0,"water":-1,"gun":1}
 final= dictonary[input]
 
-def game(snake , water, gun):
-   if(computer == 0 and final == 1):
-       return "You Win"
-   eleif(final==1 and computer==2):
-   return "i loose"
-    elif(final==2 and computer==0):
+if(computer== final):
+       print ("its a tie")
+else:     
+     if(computer == 0 and final == -1):
+       print ("comp Win")   
+     elif(computer == 0 and final == 1):
+       print ("You Win" )  
+     elif(computer == -1 and final == 0):
+      print ("you Win" )  
+     elif(computer == -1 and final == 1):
+      print ("you Win" )  
+     elif(computer == 1 and final == 0):
+       print ("comp win")   
+     elif(computer == 1 and final == -1):
+      print ("You Win" )  
+
+
+
+
+   
+   
+
+   
+  
         
 
 
