3609adbf301f5fb2188bc58659a5888cac82e388
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-09-09 19:11:59 +0530

    initial

---
+++ PracticeLoops/pr01.py
@@ -0,0 +1,11 @@
+input = (input("enter the name"))
+friends= ["Rolf", "Bob", "Jen", "Anne", "siddharth" ,"manisha"]
+for i in range(len(friends)):
+ if( input=="manisha"):
+  print(f"{input} is your lover")
+  break
+else:
+      print(f"{input} is your friend")
+      
+    
+
---
+++ PracticeLoops/pr02.py
@@ -0,0 +1,3 @@
+input= (int)(input("Enter the Number "))
+for i in range(1,11):
+    print(f"{input} * {11-i}= {input*(11-i)}")
