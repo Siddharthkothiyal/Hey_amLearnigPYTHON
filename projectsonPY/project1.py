c58bc3b3254999706b044fce3b2e6b76c77fe32f
Siddharth_Kumar_Kothiyal <siddharthkothiyal30@gmail.com>
2025-09-09 19:07:33 +0530

    first commit on it

---
+++ projectsonPY/project1.py
@@ -0,0 +1,18 @@
+
+input = (input("Enter a number: "))
+computer= -1
+dictonary={"snake":0,"water":1,"gun":2}
+final= dictonary[input]
+
+def game(snake , water, gun):
+   if(computer == 0 and final == 1):
+       return "You Win"
+   eleif(final==1 and computer==2):
+   return "i loose"
+    elif(final==2 and computer==0):
+        
+
+
+
+
+    
